2948ae0cd93a34b28bb149b4c8eb602808be60fa
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-01-30 12:37:48 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 
 def hello_world():
-
-    return "Hello World"
+    return render_template('index.html')
