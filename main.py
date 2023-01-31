f799ffd47a2a82b66347d3a08fec3e010ee5904c
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2023-01-30 18:47:40 -0800

    changes so far

--- main.py
+++ main.py
@@ -1,8 +1,107 @@
-from flask import Flask, render_template
-
+"""
+levels: {
+    levelName: {
+        name: 'levelName' # technicaly redundent but simplfies the code using this json
+        spawn: {
+            x: 0,
+            y: 0
+        }
+        entities: [
+            {
+                obj: 'player',
+                name: 'p1',
+                location: {
+                    x: 10,
+                    y: -10
+                }
+            },
+            {
+                obj: 'key',
+                name: 'Key',
+                location: {
+                    x: 15,
+                    y: 10
+                }
+            }
+        ]
+        players: {'p1': 0} # this way we dont need to comb through all the entities just to find players
+    }
+}
+"""
+from flask import Flask
+import json,time
+#now dumps is global func waw
 app = Flask(__name__)
+# contains levels
+levels = {
+    'test': {
+        'name': 'test server',
+        'spawn': {
+            'x': -135,
+            'y': -155
+        },
+        'entities': [
+            {
+                'obj': 'sign',
+                'name': 'Info',
+                'text': 'this server is to test the cubix backend',
+                'location': {
+                    'x': -130,
+                    'y': -155
+                }
+            }
+        ],
+        'players': {}
+    }
+}
+responses = {
+    'noLevel': '{"error": "requested level doesnt exist", "success": false}',
+    'alreadyJoined': '{"error": "there is already someone online with that name", "success": false}',
+    'noPlayer': '{"error": "there is noone in this server by that name", "success": false}',
+    'success': '{"success": true}'
+}
+def newObject(level, type, name, location, meta):
+    newObj = {
+        'obj': type,
+        'name': name,
+        'location': location
+    }
+    if type == 'sign':
+        newObj['text'] = meta['text']
+    if type == 'player': 
+        levels[level]['players'][name] = levels[level]['entities'].length
+    levels[level]['entities'].append(newObj)
+
+def usernameUsed(level, username):
+    return username in levels[level]['players']
+
+@app.route('/join', methods=["GET"])
+def push_user():
+    level = request.args.level
+    username = request.args.user
+    if level == None:
+        return levels.keys()
+    if not level in levels: 
+        return responses['noLevel']
+    if usernameUsed(level, username):
+        return responses['alreadyJoined']
+
+    newObject(level, 'player', username, levels[level]['spawn'])
+    return json.dumps(levels[level])
+
+@app.route('/leave', methods=["GET"])
+def remove_user():
+    level = request.args.level
+    username = request.args.user
+    if not level in levels: 
+        return responses['noLevel']
+    if not username in levels[level]['players']:
+        return responses['noPlayer']
+        
+    player = levels[level]['players'][username]
+    levels[level]['entities'].remove(player)
+    return responses['noPlayer']
 
 @app.route('/', methods=["GET"])
-
 def hello_world():
-    return render_template('index.html')
+    return 'you should not be here >:('
