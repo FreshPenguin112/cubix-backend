3ab82df6e5e61a49579967aaf4bde2076ec53433
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-02-01 09:03:57 -0600

    Update main.py

--- main.py
+++ main.py
@@ -111,10 +111,10 @@ def remove_user():
 @app.route('/levels', methods=["GET","POST"])
 def levelslol():
     try:
-        if request.method == "GET":
-            return levels
-        elif request.method == "POST":
+        if request.method == "POST":
             levels = request.form.get("data")
+        elif request.method == "GET":
+            return levels
     except BaseException as e:
         return str(e)
 @app.route('/', methods=["GET"])
