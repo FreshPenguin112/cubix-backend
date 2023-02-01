6285ef9c11600a0e3017b2145c43dc79dc93e32d
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-02-01 17:08:02 -0600

    Update main.py

--- main.py
+++ main.py
@@ -28,52 +28,53 @@ levels: {
     }
 }
 """
-from flask import Flask,request
-import json,time
-#now dumps is global func waw
+from flask import Flask, request
+import json, time
+
 app = Flask(__name__)
-# contains levels
 levels = {
     'test': {
-        'name': 'test server',
+        'name':
+        'test server',
         'spawn': {
             'x': -135,
             'y': -155
         },
-        'entities': [
-            {
-                'obj': 'sign',
-                'name': 'Info',
-                'text': 'this server is to test the cubix backend',
-                'location': {
-                    'x': -130,
-                    'y': -155
-                }
+        'entities': [{
+            'obj': 'sign',
+            'name': 'Info',
+            'text': 'this server is to test the cubix backend',
+            'location': {
+                'x': -130,
+                'y': -155
             }
-        ],
+        }],
         'players': {}
     }
 }
 responses = {
     'noLevel': '{"error": "requested level doesnt exist", "success": false}',
-    'alreadyJoined': '{"error": "there is already someone online with that name", "success": false}',
-    'noPlayer': '{"error": "there is noone in this server by that name", "success": false}',
+    'alreadyJoined':
+    '{"error": "there is already someone online with that name", "success": false}',
+    'noPlayer':
+    '{"error": "there is noone in this server by that name", "success": false}',
     'success': '{"success": true}'
 }
+
+
 def newObject(level, type, name, location, meta):
-    newObj = {
-        'obj': type,
-        'name': name,
-        'location': location
-    }
+    newObj = {'obj': type, 'name': name, 'location': location}
     if type == 'sign':
         newObj['text'] = meta['text']
-    if type == 'player': 
+    if type == 'player':
         levels[level]['players'][name] = len(levels[level]['entities'])
     levels[level]['entities'].append(newObj)
 
+
 def usernameUsed(level, username):
     return username in levels[level]['players']
+
+
 @app.route('/join', methods=["GET"])
 def push_user():
     try:
@@ -81,41 +82,47 @@ def push_user():
         username = request.args.get("user")
         if level == None:
             return levels.keys()
-        if not level in levels: 
+        if not level in levels:
             return responses['noLevel']
         if usernameUsed(level, username):
             return responses['alreadyJoined']
 
-        newObject(level, 'player', username, levels[level]['spawn'],"hi")
+        newObject(level, 'player', username, levels[level]['spawn'], "hi")
         return levels
     except BaseException as e:
         return str(e)
 
+
 @app.route('/leave', methods=["GET"])
 def remove_user():
-    try:
-        level = request.args.get("level")
-        username = request.args.get("user")
-        if not level in levels: 
-            return responses['noLevel']
-        """
-        elif not username in levels[level]['entities']:
-            return responses['noPlayer']
-        """
-        player = levels[level]['players'][username]
-        del levels[level]['players'][player]
-        return {"success": true}
-    except BaseException:
-        return responses["success"]
+    level = request.args.get("level")
+    username = request.args.get("user")
+    if not level in levels:
+        return responses['noLevel']
+    """
+    elif not username in levels[level]['entities']:
+        return responses['noPlayer']
+    """
+    player = levels[level]['players'][username]
+    levels[level]['players'].pop(player, None)
+    return responses["success"]
+
+
 @app.route('/levels', methods=["GET"])
-def levelslol(): # repl chat
+def levelslol():
     return levels
-@app.route('/levelspost',methods=["POST"])
+
+
+@app.route('/levelspost', methods=["POST"])
 def levelspost():
     global levels
-    # idk then :( 
     levels = request.form.get("data")
     return responses["success"]
+
+
 @app.route('/', methods=["GET"])
 def hello_world():
     return 'you should not be here >:('
+
+
+app.run(host="0.0.0.0", port=8080, debug=False)
